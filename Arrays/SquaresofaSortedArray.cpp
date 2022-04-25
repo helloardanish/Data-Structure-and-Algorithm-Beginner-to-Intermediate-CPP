782d12001cbc2c0a7ec60eb4891a15a786618d98
A R DANISH <helloardanish@gmail.com>
2022-04-25 05:43:05 +0530

    Update SquaresofaSortedArray.cpp

--- Arrays/SquaresofaSortedArray.cpp
+++ Arrays/SquaresofaSortedArray.cpp
@@ -42,3 +42,33 @@ public:
         return retarr;
     }
 };
+
+
+
+
+
+
+
+// no extra vector
+
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        
+        for(int i=0; i<nums.size(); i++){
+          int temp = nums[i]*nums[i];
+          nums[i] = temp;
+        }
+        
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
