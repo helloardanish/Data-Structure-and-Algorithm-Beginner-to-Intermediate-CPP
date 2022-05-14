0bb3116b5b260f5f28e2df7c1b4acf7cebf78900
A R DANISH <helloardanish@gmail.com>
2022-05-14 23:59:42 +0530

    Create 007_Count_the_Occurence_of_Element.cpp

---
+++ Recursion/Advanced_Recrusion/007_Count_the_Occurence_of_Element.cpp
@@ -0,0 +1,64 @@
+// Given an array, count how many times a given number is present.
+
+// input arr[5,5,6,7,8,6,1, 6], k =5 >> Output 2 (because 5 present at index 0 and 1 which is 2 times)
+// input arr[5,5,6,7,8,6,1, 6], k =6 >> Output 3 (because 6 present at index 2 , 5 and 7, which is total3 times)
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+int countpresentnumber(int arr[], int n, int k, int i, int count){
+  // base condition
+  if(i==n){
+    return count;
+  }
+  // calculation
+  if(arr[i]==k){
+    count++;
+  }
+  return countpresentnumber(arr, n, k, i+1, count);
+}
+int main(){
+  int n=8;
+  int arr[] ={5, 6, 6, 7, 8, 6, 8, 6};
+  int k=6;
+  int ans = countpresentnumber(arr, n, k, 0, 0);
+  cout << ans;
+  cout <<endl;
+}
+
+
+// very simple
+
+
+
+
+// pass by reference
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+void countpresentnumber(int arr[], int n, int k, int i, int &count){
+  // base condition
+  if(i==n){
+    return;
+  }
+  // calculation
+  if(arr[i]==k){
+    count++;
+  }
+  countpresentnumber(arr, n, k, i+1, count);
+}
+int main(){
+  int n=8;
+  int arr[] ={5, 6, 6, 7, 8, 6, 8, 6};
+  int k=6;
+  int count=0;
+  countpresentnumber(arr, n, k, 0, count);
+  cout << count;
+  cout <<endl;
+}
