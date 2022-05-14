2ff0117e9eed73c163eb36fd968574256c3edd2d
A R DANISH <helloardanish@gmail.com>
2022-05-14 17:34:48 +0530

    Update No_of_digit.cpp

--- Recursion/No_of_digit.cpp
+++ Recursion/No_of_digit.cpp
@@ -47,3 +47,28 @@ int main(){
   cout << countdigit(n);
   cout <<endl;
 }
+
+
+// But break condition should be equal to 0 aways when single digit number will be divided by 10. It is integer so it will not return decimal number
+
+
+#include <iostream>
+
+using namespace std; 
+
+int countdigit(int n){
+  if(n<0){
+    return -1;
+  }
+  
+  if(n==0){
+    return 0;
+  }
+  
+  return 1+countdigit(n/10);
+}
+int main(){
+  int n=1000;
+  cout << countdigit(n);
+  cout <<endl;
+}
