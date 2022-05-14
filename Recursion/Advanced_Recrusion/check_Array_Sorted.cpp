58873d8842971027ec975e45d818860666864b86
A R DANISH <helloardanish@gmail.com>
2022-05-14 21:02:22 +0530

    Update check_Array_Sorted.cpp

--- Recursion/Advanced_Recrusion/check_Array_Sorted.cpp
+++ Recursion/Advanced_Recrusion/check_Array_Sorted.cpp
@@ -68,3 +68,70 @@ int main(){
   cout << boolalpha << checksortedarray(arr, n);
   cout <<endl;
 }
+
+
+
+// first call the function and later check the condition
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+bool checksortedarray(int arr[], int n){
+  
+  // base condition
+  if(n==0 || n==1){
+    return true;
+  }
+
+  // Check condition
+  bool isSorted = checksortedarray(arr+1, n-1);
+
+  /*
+  if(arr[0] > arr[1]){
+    return false;
+  }esle{
+    return true;
+  }
+*/
+  return !(arr[0] > arr[1]);
+}
+int main(){
+  int n=4;
+  int arr[] ={5, 7, 10, 9};
+  cout << boolalpha << checksortedarray(arr, n);
+  cout <<endl;
+}
+
+
+// this will work too
+
+// we can also check last two element
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+bool checksortedarray(int arr[], int n){
+  
+  // base condition
+  if(n==0 || n==1){
+    return true;
+  }
+
+  // Check condition
+
+  if(arr[n-2] > arr[n-1]){
+    return false;
+  }
+  return checksortedarray(arr, n-1);
+}
+int main(){
+  int n=4;
+  int arr[] ={5, 7, 10, 9};
+  cout << boolalpha << checksortedarray(arr, n);
+  cout <<endl;
+}
