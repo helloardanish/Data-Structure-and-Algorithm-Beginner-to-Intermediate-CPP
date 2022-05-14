b7e78bb089f05b65625fb5193a574483565ac986
A R DANISH <helloardanish@gmail.com>
2022-05-14 22:45:09 +0530

    Update 003_Check_Elem_In_Arr.cpp

--- Recursion/Advanced_Recrusion/003_Check_Elem_In_Arr.cpp
+++ Recursion/Advanced_Recrusion/003_Check_Elem_In_Arr.cpp
@@ -24,3 +24,96 @@ int main(){
   cout << boolalpha <<checkeleinarr(arr, n, k);
   cout <<endl;
 }
+
+
+// or
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+bool checkeleinarr(int arr[], int n, int k, int i){
+  // base condition
+  if(i==n){
+    return false;
+  }
+  
+  // Check condition
+  if(arr[i]==k){
+    return true;
+  }
+  return  checkeleinarr(arr, n, k, i+1);
+}
+int main(){
+  int n=4;
+  int arr[] ={5, 7, 10, 9};
+  int k=10;
+  cout << boolalpha <<checkeleinarr(arr, n, k, 0);
+  cout <<endl;
+}
+
+
+
+// or
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+bool checkeleinarr(int arr[], int n, int k){
+  // base condition
+  if(n==0){
+    return false;
+  }
+  
+  // Check condition
+  bool remArr = checkeleinarr(arr+1, n-1, k);
+  if(remArr){
+    return true;
+  }
+  if(arr[0]==k){
+    return true;
+  }else{
+    return false;
+  }
+}
+int main(){
+  int n=4;
+  int arr[] ={5, 7, 10, 9};
+  int k=10;
+  cout << boolalpha <<checkeleinarr(arr, n, k);
+  cout <<endl;
+}
+
+
+
+// or
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+bool checkeleinarr(int arr[], int n, int k){
+  // base condition
+  if(n==0){
+    return false;
+  }
+  
+  if(arr[n-1]==k){
+    return true;
+  }
+  
+  return checkeleinarr(arr, n-1, k);
+}
+int main(){
+  int n=4;
+  int arr[] ={5, 7, 10, 9};
+  int k=15;
+  cout << boolalpha <<checkeleinarr(arr, n, k);
+  cout <<endl;
+}
