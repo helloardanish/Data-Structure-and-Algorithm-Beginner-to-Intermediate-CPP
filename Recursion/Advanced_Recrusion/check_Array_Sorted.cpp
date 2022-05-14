ed1809b67bc441a7dc97e1fb8d99aaaf97b1d173
A R DANISH <helloardanish@gmail.com>
2022-05-14 20:51:28 +0530

    Create check_Array_Sorted.cpp

---
+++ Recursion/Advanced_Recrusion/check_Array_Sorted.cpp
@@ -0,0 +1,70 @@
+// given an array check if array is sorted or not. Return true if sorted or flase when this is not sorted.
+
+// input arr = [12, 43, 54, 43] >>  return true , input arr [21,22,19,29] >> false
+
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+bool checksortedarray(int arr[], int n){
+  
+  // base condition
+  if(n==0 || n==1){
+    return true;
+  }
+
+  // For size 2
+  if(arr[0] > arr[1]){
+    return false;
+  }
+  
+  bool isSmallSorted = checksortedarray(arr+1, n-1);
+
+  /*
+  if(isSmallSorted){
+    return true;
+  }else{
+    return false;
+  }
+  */
+  return isSmallSorted;
+}
+int main(){
+  int n=4;
+  int arr[] ={5, 7, 8, 9};
+  cout << boolalpha << checksortedarray(arr, n);
+  cout <<endl;
+}
+
+
+
+// or
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+bool checksortedarray(int arr[], int n){
+  
+  // base condition
+  if(n==0 || n==1){
+    return true;
+  }
+
+  // Check condition
+  if(arr[0] > arr[1]){
+    return false;
+  }
+  
+  return checksortedarray(arr+1, n-1);;
+}
+int main(){
+  int n=4;
+  int arr[] ={5, 7, 10, 9};
+  cout << boolalpha << checksortedarray(arr, n);
+  cout <<endl;
+}
