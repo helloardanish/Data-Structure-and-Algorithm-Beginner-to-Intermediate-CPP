25792e46d7e2099251e231b8e969bf4ee50ef89d
A R DANISH <helloardanish@gmail.com>
2022-05-15 20:43:37 +0530

    Update 006_Quick_Sort.cpp

--- Sorting_and_Searching/006_Quick_Sort.cpp
+++ Sorting_and_Searching/006_Quick_Sort.cpp
@@ -50,11 +50,153 @@ int main(){
 
 
 
+// using first index as pivot
+
+
+#include <iostream>
+
+using namespace std; 
+
+int findpivotindex(int nums[], int s, int e)
+{
+ 
+    int pivot = nums[s];
+ 
+    int count = 0;
+    for (int i = s + 1; i <= e; i++) {
+        if (nums[i] <= pivot)
+            count++;
+    }
+ 
+    // Giving pivot element its correct position
+    int pivotIndex = s + count;
+    swap(nums[pivotIndex], nums[s]);
+ 
+    // Sorting left and right parts of the pivot element
+    int i = s, j = e;
+ 
+    while (i < pivotIndex && j > pivotIndex) {
+ 
+        while (nums[i] <= pivot) {
+            i++;
+        }
+ 
+        while (nums[j] > pivot) {
+            j--;
+        }
+ 
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(nums[i++], nums[j--]);
+        }
+    }
+ 
+    return pivotIndex;
+}
+
+void quickSort(int nums[], int s, int e){
+  // base condition
+  if(s>=e){
+    return;
+  }
+  int pivot = findpivotindex(nums,s,e);
+  
+  // call for before pivot array
+  quickSort(nums,s,pivot-1);
+
+  // call for after pivot array
+  quickSort(nums,pivot+1, e);  
+}
+
+int main(){
+  int nums[]={1,3,5,9,2,7,8,6,4};
+  int n=9;
+  quickSort(nums, 0, n-1);
+  for(auto x:nums){
+    cout << x << " ";
+  }
+  cout << endl;
+}
+
+
+
+
+
+// using last element as pivot
+
+
+
+#include <iostream>
+
+using namespace std; 
+
+int findpivotindex(int nums[], int s, int e)
+{
+ 
+    int pivot = nums[e];
+ 
+    int count = 0;
+    for (int i = s ; i <= e-1; i++) {
+        if (nums[i] <= pivot)
+            count++;
+    }
+ 
+    // Giving pivot element its correct position
+    int pivotIndex = s + count;
+    swap(nums[pivotIndex], nums[e]);
+ 
+    // Sorting left and right parts of the pivot element
+    int i = s, j = e;
+ 
+    while (i < pivotIndex && j > pivotIndex) {
+ 
+        while (nums[i] <= pivot) {
+            i++;
+        }
+ 
+        while (nums[j] > pivot) {
+            j--;
+        }
+ 
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(nums[i++], nums[j--]);
+        }
+    }
+ 
+    return pivotIndex;
+}
+
+void quickSort(int nums[], int s, int e){
+  // base condition
+  if(s>=e){
+    return;
+  }
+  int pivot = findpivotindex(nums,s,e);
+  
+  // call for before pivot array
+  quickSort(nums,s,pivot-1);
+
+  // call for after pivot array
+  quickSort(nums,pivot+1, e);  
+}
+
+int main(){
+  int nums[]={1,3,5,9,2,7,8,6,4};
+  int n=9;
+  quickSort(nums, 0, n-1);
+  for(auto x:nums){
+    cout << x << " ";
+  }
+  cout << endl;
+}
+
 
 
 /*
-will solve this way later, brain ****** up
+At the starting I was solving this way
 
+brain ****** up
+
+after 2 hours finally coded out above solution
 
 #include <iostream>
 
