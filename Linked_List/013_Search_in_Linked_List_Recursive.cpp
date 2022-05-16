18326d54b1402e95497ebe270afd0841ed27b914
A R DANISH <helloardanish@gmail.com>
2022-05-17 01:50:02 +0530

    Create 013_Search_in_Linked_List_Recursive.cpp

---
+++ Linked_List/013_Search_in_Linked_List_Recursive.cpp
@@ -0,0 +1,82 @@
+// search a node in linked list, recursively
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+
+class Node{
+  public:
+    int data;
+    Node *next;
+    Node(int data){
+      this->data = data;
+      next = NULL;
+    }
+};
+
+void printLL(Node * head){ // if you call this before lenght, results will be zero
+  while(head!=NULL){
+    int datavalue = head->data;
+    cout << datavalue <<" ";
+    head = head->next;
+  }
+  cout << "\nNULL" << endl;
+  return;
+}
+
+
+
+
+Node * takingInput(){
+  int inp;
+  cin >> inp;
+  // assume linkedlist is empty
+  Node * head = NULL;
+  Node * tail = NULL;
+  while(inp!=-1){
+
+    /*
+    Node n(inp); // static, but it will be lost in next call
+    we have to create dynamic node 
+    */
+    Node *n = new Node(inp);
+    if(head==NULL){
+      head = n;
+      tail = n;
+    }else{
+      tail->next = n;
+      tail = tail ->next;
+      // no need to update tail as first value will always be the tail
+    }
+    cin >> inp;
+  }
+  return head;
+}
+
+
+bool searchLinkedList(Node * head, int k){
+  
+  if(head==NULL){
+    return false;
+  }
+  if(head->data==k){
+    return true;
+  }else{
+    return searchLinkedList(head->next, k);
+  }
+}
+
+
+int main(){
+
+  Node * head = takingInput();
+  int k;
+  cin>>k;
+  bool ans = searchLinkedList(head, k);
+  cout << boolalpha << ans << endl;
+  cout << endl;
+}
+
