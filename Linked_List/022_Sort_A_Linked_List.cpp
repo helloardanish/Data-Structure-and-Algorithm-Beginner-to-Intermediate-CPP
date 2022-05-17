89e1eb4bef8d803ca60f8e0b4319148d7a60ea21
A R DANISH <helloardanish@gmail.com>
2022-05-17 14:22:29 +0530

    Update 022_Sort_A_Linked_List.cpp

--- Linked_List/022_Sort_A_Linked_List.cpp
+++ Linked_List/022_Sort_A_Linked_List.cpp
@@ -80,3 +80,57 @@ public:
         return finalHead;
     }
 };
+
+
+
+// We can also add two linked list recursively
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL) return list2;
+        if(list2==NULL) return list1;
+        
+        ListNode * newHead = NULL;
+        if(list1->val < list2->val){
+            newHead = list1;
+            newHead->next = mergeTwoLists(list1->next, list2);
+        }else{
+            newHead = list2;
+            newHead->next = mergeTwoLists(list1, list2->next);
+        }
+        return newHead;
+    }
+    
+    
+    
+    ListNode* sortList(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL) return head;
+        ListNode * slow = head;
+        ListNode * fast = head->next;
+        ListNode * copyHead = head; // copying head just for safe
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode *n = slow->next; // slow->next to NULL second half
+        slow->next = NULL; // head to slow is first half
+        ListNode * firstHalfLL = sortList(copyHead); // First Half
+        ListNode * secondHalfLL = sortList(n); // Second half
+        // we have to merge both the list
+        ListNode * finalHead = mergeTwoLists(firstHalfLL, secondHalfLL);
+        return finalHead;
+    }
+};
