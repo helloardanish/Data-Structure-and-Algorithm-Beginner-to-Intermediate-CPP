6e74a96e6e2b400fec668a7b3dc5b12939672fd5
A R DANISH <helloardanish@gmail.com>
2022-05-21 16:52:14 +0530

    Create 003_Take_Input_Recursively_Binary_Tree.cpp

---
+++ Binary_Tree/003_Take_Input_Recursively_Binary_Tree.cpp
@@ -0,0 +1,103 @@
+// Taking input recursively in binary tree
+
+
+// Taking input in Binary tree recursively
+
+
+
+#include <iostream>
+
+
+using namespace std;
+
+template<typename T>
+class BTNode{
+
+  public:
+    T data;
+    BTNode * left;
+    BTNode * right;
+
+  public:
+    BTNode(T data){
+      this->data = data;
+      left = NULL;
+      right=NULL;
+    }
+
+    //recursive  destructor
+    ~BTNode(){
+      delete left; // delete NULL means nothing when no root has neither left nor right child
+      delete right;
+    }
+
+};
+
+
+void printTree(BTNode<int> * root){
+  if(root==NULL){//base case otherwise it will call null and give segmentation fault
+    return;
+  }
+  
+  cout << root->data << " : ";
+  if(root->left!=NULL){
+    cout << "L" << root->left->data;
+  }
+
+  cout << " ";
+  
+  if(root->right!=NULL){
+    cout << "R" << root->right->data;
+  }
+  
+  cout << endl;
+  printTree(root->left);
+  printTree(root->right);
+}
+
+
+BTNode<int> * takingInput(){
+  // using -1 as terminator
+  int rootdata;
+  cin>>rootdata;
+  if(rootdata==-1){
+    return NULL;
+  }
+  BTNode<int> * root = new BTNode<int>(rootdata);
+
+  BTNode<int> * leftChild = takingInput();
+  
+  BTNode<int> * rightChild = takingInput();
+
+  root->left = leftChild;
+  root->right = rightChild;
+  
+  return root;
+}
+
+int main(){
+  BTNode<int> * root = takingInput();
+  
+  printTree(root);
+  
+  delete root;
+  return 0;
+}
+
+
+/* input and output
+
+
+1
+2
+-1
+-1
+3
+-1
+-1
+1 : L2 R3
+2 :  
+3 :
+
+
+*/
