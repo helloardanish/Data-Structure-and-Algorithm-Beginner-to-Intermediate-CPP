87186cd543e9b9c370701a0eeb10f3fba24c47b5
A R DANISH <helloardanish@gmail.com>
2022-05-23 18:53:51 +0530

    Update 018_Diameter_of_Binary_Tree.cpp

--- Binary_Tree/018_Diameter_of_Binary_Tree.cpp
+++ Binary_Tree/018_Diameter_of_Binary_Tree.cpp
@@ -38,3 +38,56 @@ public:
         return max(option1, max(option2, option3));
     }
 };
+
+
+
+// Time complexity = O(n*h)
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int,int> heightDiameter(TreeNode * root){
+        if(root==NULL){
+            pair<int,int> p;
+            p.first = 0;
+            p.second = 0;
+            return p;
+        }
+        
+        pair<int,int> leftAns = heightDiameter(root->left);
+        pair<int,int> rightAns = heightDiameter(root->right);
+        
+        int lh = leftAns.first;
+        int ld = leftAns.second;
+        
+        int rh = rightAns.first;
+        int rd = rightAns.second;
+        
+        int height = 1 + max(lh, rh);
+        int diameter = max(lh+rh, max(ld, rd));
+        
+        pair<int,int> p;
+        p.first = height;
+        p.second = diameter;
+        return p;
+    }
+    
+    int diameterOfBinaryTree(TreeNode* root) {
+        pair<int,int> p = heightDiameter(root);
+        return p.second;
+    }
+};
+
+// TC = O(n), better approach
