777fdc693baa20bcbc1cc4e7263307c37f265219
A R DANISH <helloardanish@gmail.com>
2022-05-27 03:39:54 +0530

    Create 001_Adjacency_Matrix.cpp

---
+++ Graph/001_Adjacency_Matrix.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+   int n,e;
+   cin>>n>>e;
+
+   vector<vector<int> > matrix(n,vector<int>(n,0));
+
+   for(int i=1;i<=e;i++){
+    int fv,sv;
+    cin>>fv>>sv;
+    matrix[fv][sv] = 1;
+    matrix[sv][fv] = 1;
+   }
+   
+   return 0;
+}
